f8c9cc95ed4fd362cb00e356c121d7fb2b68eea1
“helal28d” <helal.dahi1992@gmail.com>
2023-06-13 08:39:03 -0500

    home api

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -11,7 +11,9 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
+      const res = await axios.get(
+        "https://react-blog-api-ten.vercel.app/posts" + search
+      );
       setPosts(res.data);
       console.log(res.data);
     };
